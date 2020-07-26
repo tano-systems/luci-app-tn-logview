tano-systems/luci-app-tn-logview

commit b1b1146e05a9ec2215b7e486d15352ce3aafb62e

Replace fs.read_direct and fs.exec_direct to ubus versions
This is did because cgi-io has a 128 KiB limit for POST requests.
Accordingly, when loading large logs, everything is crashes.

Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

htdocs/luci-static/resources/logview/logview.js
@@ -185,16 +185,16 @@ function logviewTableUpdate(plugin, logData, filterPattern) {
 	plugin.opts.data = logData;
 }
 
-function getActionTask(action, type) {
+function getActionTask(action) {
 	var task = null;
 
 	if (!action)
 		return null;
 
 	if (action.hasOwnProperty('command') && action.command) {
-		task = fs.exec_direct(action.command, action.command_args || [], type);
+		task = fs.exec(action.command, action.command_args || []);
 	} else if (action.hasOwnProperty('file') && action.file) {
-		task = fs.read_direct(action.file, type);
+		task = fs.read(action.file, type);
 	}
 
 	return task;
@@ -356,8 +356,9 @@ return L.Class.extend({
 			])
 		]);
 
-		return Promise.resolve(getActionTask(plugin.json_data.action, 'json')).then(function(data) {
-			logviewTableUpdate(plugin, data, filterPattern);
+		return Promise.resolve(getActionTask(plugin.json_data.action)).then(function(data) {
+			var json = JSON.parse(data.stdout);
+			logviewTableUpdate(plugin, json, filterPattern);
 		});
 	},
 
@@ -383,8 +384,9 @@ return L.Class.extend({
 		else
 			download = plugin.downloads[downloadName];
 
-		return Promise.resolve(getActionTask(download.action, 'blob')).then(function(res) {
-			var url = URL.createObjectURL(new Blob([res], {
+		return Promise.resolve(getActionTask(download.action)).then(function(res) {
+			var data = res.stdout;
+			var url = URL.createObjectURL(new Blob([data], {
 				type: download.mime_type
 			}));
 

commit 3ed2e34f6f8465bfab9d3934eb3a442b9ec08c72

Return back fs.read_direct and fs.exec_direct
I don't remember the reason why fs.exec_direct and fs.read_direct
have been replaced by the ubus versions, but this is not correct.

Fixes: b1b1146 ("Replace fs.read_direct and fs.exec_direct to ubus versions")
Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

htdocs/luci-static/resources/logview/logview.js
@@ -246,9 +246,9 @@ function getActionTask(action) {
 		return null;
 
 	if (action.hasOwnProperty('command') && action.command) {
-		task = fs.exec(action.command, action.command_args || []);
+		task = fs.exec_direct(action.command, action.command_args || []);
 	} else if (action.hasOwnProperty('file') && action.file) {
-		task = fs.read(action.file, type);
+		task = fs.read_direct(action.file, type);
 	}
 
 	return task;
@@ -426,9 +426,9 @@ return L.Class.extend({
 		return Promise.resolve(getActionTask(plugin.json_data.action)).then(function(data) {
 			var json;
 
-			if (data.code == 0) {
+			if (data) {
 				try {
-					var json = JSON.parse(data.stdout);
+					var json = JSON.parse(data);
 					plugin.opts.load_error = null;
 				}
 				catch(e) {
@@ -443,9 +443,9 @@ return L.Class.extend({
 			else {
 				json = [];
 				plugin.opts.load_error = {
-					code: data.code,
-					stdout: data.stdout,
-					stderr: data.stderr
+					code: -1,
+					stdout: '',
+					stderr: _('No data')
 				};
 			}
 
@@ -477,8 +477,7 @@ return L.Class.extend({
 			download = plugin.downloads[downloadName];
 
 		return Promise.resolve(getActionTask(download.action)).then(function(res) {
-			var data = res.stdout;
-			var url = URL.createObjectURL(new Blob([data], {
+			var url = URL.createObjectURL(new Blob([res], {
 				type: download.mime_type
 			}));
 